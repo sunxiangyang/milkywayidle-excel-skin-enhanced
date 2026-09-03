3c2d032a95e71609ca77a9f9e6b30e4ac8d48f90
shentianqi <asd@163.com>
2026-09-03 17:25:38 +0800

    递增脚本版本至1.0.5.2

--- milkywayidle-excel.user.js
+++ milkywayidle-excel.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   MilkyWayIdle - Excel换肤增强版
 // @namespace    https://github.com/ailec0623/MilkyWayIdle-FullscreenIDEChat
 // @description  游戏界面右下角按钮启动。快捷键alt + I (MacOS: cmd + I)切换为Excel模式。支持多种配色和图标显隐。
-// @version      1.0.5.1
+// @version      1.0.5.2
 // @author       xiaokeai
 // @copyright    400BadRequest
 // @license      MIT
